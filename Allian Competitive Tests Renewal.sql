a4f133aa751b50330383845cda458745a0611da1
Adams Zequi <adamszeq@theaa.ie>
2022-12-04 12:28:12 +0000

    manual commit

--- Allian Competitive Tests Renewal.sql
+++ Allian Competitive Tests Renewal.sql
@@ -1,95 +1,51 @@
 SELECT   distinct homerenewal. Customerid
 
-
-  -- ,ROW_NUMBER() OVER (ORDER BY StartDate) AS Number
-  -- ,homerenewal.StartDate as CoverDate
   ,cast(homerenewal.StartDate as date) as CoverDate
 
   ,homerenewal.BuildingsCoverValue as Building_Sum_Insured
-  ,NULL as Buildings_AD
+  ,case when homerenewal.PolicyAddOn = 'Buildings Accidental Damage' then 'Y' else 'N' end as Buildings_AD
   ,homerenewal.ContentsCoverValue as Contents_Sum_Insured
-  ,NULL as Contents_AD
+  ,case when homerenewal.PolicyAddOn = 'Buildings Accidental Damage' then 'Y' else 'N' end as Contents_AD
+
+  
+
 
   ,homerenewal.VoluntaryExcess as Voluntary_Excess
   ,homerenewal.StandardCompulsoryExcess as Standard_Excess
   ,homerenewal.NonStandardCompulsoryExcess as Non_Standard_Excess
   ,NULL as Policy_Excess
-  -- ,homerenewal.VoluntaryExcess + homerenewal.StandardCompulsoryExcess + homerenewal.NonStandardCompulsoryExcess as Policy_Excess
-  --convert homerenewal.VoluntaryExcess to number and add to the other two
--- BuildingName
 
   ,homerenewal.YearsClaimFree as YearsClaimFree
 
-  
-  -- ,homerenewal.FirstClaimType as ClaimType1
   ,CASE WHEN cast(cast(homerenewal.FirstClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homerenewal.FirstClaimType END as ClaimType1
   ,CASE WHEN cast(cast(homerenewal.FirstClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homerenewal.FirstClaimDate END as ClaimDate1
   ,CASE WHEN cast(cast(homerenewal.FirstClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homerenewal.FirstClaimAmount END as ClaimAmount1
   ,CASE WHEN cast(cast(homerenewal.FirstClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homerenewal.FirstClaimPropertyOccurredAt END as ClaimRelatesToProperty1
   ,CASE WHEN cast(cast(homerenewal.FirstClaimDate as char(8)) as datetime) > '2020-01-01' THEN  (CASE WHEN homerenewal.FirstClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END) ELSE NULL END as ClaimSettled1
-  -- ,homerenewal.BuildingUnitNumber + ' ' + homerenewal.BuildingName + ' ' + homerenewal.StreetName 
-  -- + ' ' + homerenewal.Town + ' ' + homerenewal.County + ' ' + homerenewal.Postcode as ClaimsToProperty1  
 
-  -- ,homerenewal.SecondClaimType as ClaimType2
-  -- ,homerenewal.SecondClaimDate as ClaimDate2
-  -- ,homerenewal.SecondClaimAmount as ClaimAmount2
-  -- ,homerenewal.SecondClaimPropertyOccurredAt as ClaimRelatesToProperty2
-  -- ,CASE WHEN homerenewal.SecondClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END as ClaimSettled2
-  -- ,homerenewal.BuildingUnitNumber + ' ' + homerenewal.BuildingName + ' ' + homerenewal.StreetName
-  -- + ' ' + homerenewal.Town + ' ' + homerenewal.County + ' ' + homerenewal.Postcode as ClaimsToProperty2 
   ,CASE WHEN cast(cast(homerenewal.SecondClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homerenewal.SecondClaimType END as ClaimType2
   ,CASE WHEN cast(cast(homerenewal.SecondClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homerenewal.SecondClaimDate  END as ClaimDate2
   ,CASE WHEN cast(cast(homerenewal.SecondClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homerenewal.SecondClaimAmount END as ClaimAmount2
   ,CASE WHEN cast(cast(homerenewal.SecondClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homerenewal.SecondClaimPropertyOccurredAt END as ClaimRelatesToProperty2
-  -- ,CASE WHEN cast(cast(homerenewal.SecondClaimDate as char(8)) as datetime) < '2021-01-01' THEN NULL ELSE CASE WHEN homerenewal.SecondClaimStatus = 'Settled' THEN 'Y' ELSE 'NULL' END END as ClaimSettled2
   ,CASE WHEN cast(cast(homerenewal.SecondClaimDate as char(8)) as datetime) > '2020-01-01' THEN  (CASE WHEN homerenewal.SecondClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END) ELSE NULL END as ClaimSettled2
-  --if the claim date is before 2021-01-01 then set column to null else set to homerenewal.SecondClaimStatus 
 
-
-  -- ,CASE WHEN cast(cast(homerenewal.ThirdClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homerenewal.ThirdClaimType END as ClaimType3
-  -- ,CASE WHEN cast(cast(homerenewal.ThirdClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homerenewal.ThirdClaimDate  END as ClaimDate3
-  -- ,CASE WHEN cast(cast(homerenewal.ThirdClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homerenewal.ThirdClaimAmount END as ClaimAmount3
-  -- ,CASE WHEN cast(cast(homerenewal.ThirdClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homerenewal.ThirdClaimPropertyOccurredAt END as ClaimRelatesToProperty3
-  -- -- ,CASE WHEN cast(cast(homerenewal.ThirdClaimDate as char(8)) as datetime) < '2021-01-01' THEN NULL ELSE CASE WHEN homerenewal.ThirdClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END END as ClaimSettled3
-  -- ,CASE WHEN cast(cast(homerenewal.ThirdClaimDate as char(8)) as datetime) > '2020-01-01' THEN  (CASE WHEN homerenewal.ThirdClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END) ELSE NULL END as ClaimSettled3
-
-
-  -- -- ,homerenewal.FourthClaimType as ClaimType4
-  -- ,homerenewal.FourthClaimDate as ClaimDate4
-  -- ,homerenewal.FourthClaimAmount as ClaimAmount4
-  -- ,homerenewal.FourthClaimPropertyOccurredAt as ClaimRelatesToProperty4
-  -- ,CASE WHEN homerenewal.FourthClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END as ClaimSettled4
-  -- -- ,homerenewal.BuildingUnitNumber + ' ' + homerenewal.BuildingName + ' ' + homerenewal.StreetName
-  -- -- + ' ' + homerenewal.Town + ' ' + homerenewal.County + ' ' + homerenewal.Postcode as ClaimsToProperty4
-
-  -- ,homerenewal.FifthClaimType as ClaimType5
-  -- ,homerenewal.FifthClaimDate as ClaimDate5
-  -- ,homerenewal.FifthClaimAmount as ClaimAmount5
-  -- ,homerenewal.FifthClaimPropertyOccurredAt as ClaimRelatesToProperty5
-  -- ,CASE WHEN homerenewal.FifthClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END as ClaimSettled5
-
-  -- ,homerenewal.NeighbourhoodWatchInArea as Neighbourhood_Watch
-  --if homerenewal.NeighbourhoodWatchInArea = '1' then 'Y' else '0' end as Neighbourhood_Watch
   ,CASE WHEN homerenewal.NeighbourhoodWatchInArea = '1' THEN 'Y' ELSE 'N' END as Neighbourhood_Watch
   ,homerenewal.NumberOfSmokeAlarms as Smoke_Detectors
-    --if homerenewal.NumberOfSmokeAlarms = ' NSAI Approved Installer to EN50131 Standard to Central Station' or 'Installed to EN50131 Standard (Not Connected to Central Station)' or 'PSA/NSAI Approved Installer to EN50131 Standard to Central Station' 
-  --then 'Standard Alarm' else if 'other' then 'Other' else null end as Alarm_Type
+ 
   ,CASE WHEN homerenewal.RoofConstructionType = 'NSAI Approved Installer to EN50131 Standard to Central Station' THEN 'Standard Alarm' 
   WHEN homerenewal.AlarmType = 'Installed to EN50131 Standard (Not Connected to Central Station)' THEN 'Standard Alarm' 
   WHEN homerenewal.AlarmType = 'PSA/NSAI Approved Installer to EN50131 Standard to Central Station' THEN 'Standard Alarm' 
   WHEN homerenewal.AlarmType = 'Other' THEN 'Other' 
   ELSE NULL END as Alarm
-  -- ,homerenewal.Locks as Locks
-  --if locks is 1 then 'Y' else 'N' end as Locks
+
   ,CASE WHEN homerenewal.Locks = '1' THEN 'Y' ELSE 'N' END as Locks
 
    ,homerenewal.BuildingUnitNumber + ' ' + homerenewal.BuildingNumber + ' ' + homerenewal.StreetName
   + ' ' + homerenewal.Town + ' ' + homerenewal.County  as Risk_Address
-  
   ,homerenewal.Postcode as  Eircode
   ,homerenewal.County as County
 
-  ,customerdim.DateOfBirth as Date_of_Birth
+  ,homerenewal.DateOfBirth as Date_of_Birth
 
   ,NULL as Occupation
   ,NULL as Employment_Status
@@ -119,5 +75,5 @@ SELECT   distinct homerenewal. Customerid
 
 
   FROM [OP].[OP].[RenewalHomeMonitor] homerenewal
-  LEFT JOIN [EDW_PROD].[dbo].[CustomerDim] customerdim ON homerenewal.CustomerId = customerdim.CustomerId
   WHERE StartDate >= '2022-10-01' AND StartDate < '2022-11-30'
+  -- where homerenewal.PolicyAddOn = 'Contents Accidental Damage' or homerenewal.PolicyAddOn = 'Buildings Accidental Damage'
--- Allian Competitive Tests.sql
+++ Allian Competitive Tests.sql
@@ -1,113 +1,40 @@
 SELECT   distinct homemonitor. Customerid
-
-
-  -- ,ROW_NUMBER() OVER (ORDER BY StartDate) AS Number
-  -- ,homemonitor.StartDate as CoverDate
+  ,homemonitor.FirstName as First_Name
+  ,homemonitor.LastName as Last_Name
+  ,homemonitor.IsSale as Is_Sale
   ,cast(homemonitor.StartDate as date) as CoverDate
+  
 
   ,homemonitor.BuildingsCoverValue as Building_Sum_Insured
-  ,NULL as Buildings_AD
+  ,case when homemonitor.PolicyAddOn = 'Buildings Accidental Damage' then 'Y' else 'N' end as Buildings_AD
+
   ,homemonitor.ContentsCoverValue as Contents_Sum_Insured
-  ,NULL as Contents_AD
+  ,case when homemonitor.PolicyAddOn = 'Contents Accidental Damage' then 'Y' else 'N' end as Contents_AD
+  -- ,homemonitor.PolicyAddOn as Contents_AD
 
   ,homemonitor.VoluntaryExcess as Voluntary_Excess
   ,homemonitor.StandardCompulsoryExcess as Standard_Excess
   ,homemonitor.NonStandardCompulsoryExcess as Non_Standard_Excess
   ,NULL as Policy_Excess
-  -- ,homemonitor.VoluntaryExcess + homemonitor.StandardCompulsoryExcess + homemonitor.NonStandardCompulsoryExcess as Policy_Excess
-  --convert homemonitor.VoluntaryExcess to number and add to the other two
-
 
   ,homemonitor.YearsClaimFree as YearsClaimFree
-  -- ,homemonitor.FirstClaimType as ClaimType1
+
   ,CASE WHEN cast(cast(homemonitor.FirstClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homemonitor.FirstClaimType END as ClaimType1
   ,CASE WHEN cast(cast(homemonitor.FirstClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homemonitor.FirstClaimDate END as ClaimDate1
   ,CASE WHEN cast(cast(homemonitor.FirstClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homemonitor.FirstClaimAmount END as ClaimAmount1
   ,CASE WHEN cast(cast(homemonitor.FirstClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homemonitor.FirstClaimPropertyOccurredAt END as ClaimRelatesToProperty1
   ,CASE WHEN cast(cast(homemonitor.FirstClaimDate as char(8)) as datetime) > '2020-01-01' THEN  (CASE WHEN homemonitor.FirstClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END) ELSE NULL END as ClaimSettled1
-  -- ,homemonitor.BuildingUnitNumber + ' ' + homemonitor.BuildingName + ' ' + homemonitor.StreetName 
-  -- + ' ' + homemonitor.Town + ' ' + homemonitor.County + ' ' + homemonitor.Postcode as ClaimsToProperty1  
 
-  -- ,homemonitor.SecondClaimType as ClaimType2
-  -- ,homemonitor.SecondClaimDate as ClaimDate2
-  -- ,homemonitor.SecondClaimAmount as ClaimAmount2
-  -- ,homemonitor.SecondClaimPropertyOccurredAt as ClaimRelatesToProperty2
-  -- ,CASE WHEN homemonitor.SecondClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END as ClaimSettled2
-  -- ,homemonitor.BuildingUnitNumber + ' ' + homemonitor.BuildingName + ' ' + homemonitor.StreetName
-  -- + ' ' + homemonitor.Town + ' ' + homemonitor.County + ' ' + homemonitor.Postcode as ClaimsToProperty2 
   ,CASE WHEN cast(cast(homemonitor.SecondClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homemonitor.SecondClaimType END as ClaimType2
   ,CASE WHEN cast(cast(homemonitor.SecondClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homemonitor.SecondClaimDate  END as ClaimDate2
   ,CASE WHEN cast(cast(homemonitor.SecondClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homemonitor.SecondClaimAmount END as ClaimAmount2
   ,CASE WHEN cast(cast(homemonitor.SecondClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homemonitor.SecondClaimPropertyOccurredAt END as ClaimRelatesToProperty2
-  -- ,CASE WHEN cast(cast(homemonitor.SecondClaimDate as char(8)) as datetime) < '2021-01-01' THEN NULL ELSE CASE WHEN homemonitor.SecondClaimStatus = 'Settled' THEN 'Y' ELSE 'NULL' END END as ClaimSettled2
   ,CASE WHEN cast(cast(homemonitor.SecondClaimDate as char(8)) as datetime) > '2020-01-01' THEN  (CASE WHEN homemonitor.SecondClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END) ELSE NULL END as ClaimSettled2
-  --if the claim date is before 2021-01-01 then set column to null else set to homemonitor.SecondClaimStatus 
-
-
-  -- ,CASE WHEN cast(cast(homemonitor.ThirdClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homemonitor.ThirdClaimType END as ClaimType3
-  -- ,CASE WHEN cast(cast(homemonitor.ThirdClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homemonitor.ThirdClaimDate  END as ClaimDate3
-  -- ,CASE WHEN cast(cast(homemonitor.ThirdClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homemonitor.ThirdClaimAmount END as ClaimAmount3
-  -- ,CASE WHEN cast(cast(homemonitor.ThirdClaimDate as char(8)) as datetime) < '2020-01-01' THEN NULL ELSE homemonitor.ThirdClaimPropertyOccurredAt END as ClaimRelatesToProperty3
-  -- -- ,CASE WHEN cast(cast(homemonitor.ThirdClaimDate as char(8)) as datetime) < '2021-01-01' THEN NULL ELSE CASE WHEN homemonitor.ThirdClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END END as ClaimSettled3
-  -- ,CASE WHEN cast(cast(homemonitor.ThirdClaimDate as char(8)) as datetime) > '2020-01-01' THEN  (CASE WHEN homemonitor.ThirdClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END) ELSE NULL END as ClaimSettled3
-
-
-
--- --  cast(cast(r1.StartDateId as char(8)) as datetime)
---   -- ,homemonitor.FirstClaimType as ClaimType1
---    --if  convert to date(homemonitor.FirstClaimDate) is <  2021-01-01 then set to null else set to homemonitor.FirstClaimType
---   ,CASE WHEN cast(cast(homemonitor.FirstClaimDate as char(8)) as datetime) < '2021-01-01' THEN NULL ELSE homemonitor.FirstClaimType END as ClaimType1
-
-
---   -- -- ,CASE WHEN homemonitor.FirstClaimDate < '2021-01-01' THEN NULL ELSE homemonitor.FirstClaimType END as ClaimType1
---   -- --if homemonitor.FirstClaimDate is < 2021-01-01 then set to null else set to homemonitor.ClaimType1
---   ,CASE WHEN cast(cast(homemonitor.FirstClaimDate as char(8)) as datetime) < '2021-01-01' THEN NULL ELSE homemonitor.FirstClaimDate END as ClaimDate1
---   -- --if homemonitor.FirstClaimDate is < 2021-01-01 then set to null else set to homemonitor.ClaimAmount1
---   ,CASE WHEN cast(cast(homemonitor.FirstClaimDate as char(8)) as datetime) < '2021-01-01' THEN NULL ELSE homemonitor.FirstClaimAmount END as ClaimAmount1
---   -- -- ,homemonitor.FirstClaimDate as ClaimDate1
---   -- -- ,homemonitor.FirstClaimAmount as ClaimAmount1
---   -- -- ,homemonitor.FirstClaimPropertyOccurredAt as ClaimRelatesToProperty1
---   ,CASE WHEN cast(cast(homemonitor.FirstClaimDate as char(8)) as datetime) < '2021-01-01' THEN NULL ELSE homemonitor.FirstClaimPropertyOccurredAt END as ClaimRelatesToProperty1
---   -- -- ,CASE WHEN homemonitor.FirstClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END as ClaimSettled1
---   ,CASE WHEN cast(cast(homemonitor.FirstClaimDate as char(8)) as datetime) < '2021-01-01' THEN NULL ELSE CASE WHEN homemonitor.FirstClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END END as ClaimSettled1
  
-
-  -- ,homemonitor.SecondClaimType as ClaimType2
-  -- ,homemonitor.SecondClaimDate as ClaimDate2
-  -- ,homemonitor.SecondClaimAmount as ClaimAmount2
-  -- ,homemonitor.SecondClaimPropertyOccurredAt as ClaimRelatesToProperty2
-  -- ,CASE WHEN homemonitor.SecondClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END as ClaimSettled2
-  -- -- ,homemonitor.BuildingUnitNumber + ' ' + homemonitor.BuildingName + ' ' + homemonitor.StreetName
-  -- -- + ' ' + homemonitor.Town + ' ' + homemonitor.County + ' ' + homemonitor.Postcode as ClaimsToProperty2 
-
-  -- ,homemonitor.ThirdClaimType as ClaimType3
-  -- ,homemonitor.ThirdClaimDate as ClaimDate3
-  -- ,homemonitor.ThirdClaimAmount as ClaimAmount3
-  -- ,homemonitor.ThirdClaimPropertyOccurredAt as ClaimRelatesToProperty3
-  -- ,CASE WHEN homemonitor.ThirdClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END as ClaimSettled3
-  -- -- ,homemonitor.BuildingUnitNumber + ' ' + homemonitor.BuildingName + ' ' + homemonitor.StreetName
-  -- -- + ' ' + homemonitor.Town + ' ' + homemonitor.County + ' ' + homemonitor.Postcode as ClaimsToProperty3
-
-  -- ,homemonitor.FourthClaimType as ClaimType4
-  -- ,homemonitor.FourthClaimDate as ClaimDate4
-  -- ,homemonitor.FourthClaimAmount as ClaimAmount4
-  -- ,homemonitor.FourthClaimPropertyOccurredAt as ClaimRelatesToProperty4
-  -- ,CASE WHEN homemonitor.FourthClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END as ClaimSettled4
-  -- -- ,homemonitor.BuildingUnitNumber + ' ' + homemonitor.BuildingName + ' ' + homemonitor.StreetName
-  -- -- + ' ' + homemonitor.Town + ' ' + homemonitor.County + ' ' + homemonitor.Postcode as ClaimsToProperty4
-
-  -- ,homemonitor.FifthClaimType as ClaimType5
-  -- ,homemonitor.FifthClaimDate as ClaimDate5
-  -- ,homemonitor.FifthClaimAmount as ClaimAmount5
-  -- ,homemonitor.FifthClaimPropertyOccurredAt as ClaimRelatesToProperty5
-  -- ,CASE WHEN homemonitor.FifthClaimStatus = 'Settled' THEN 'Y' ELSE 'N' END as ClaimSettled5
-
-  -- ,homemonitor.NeighbourhoodWatchInArea as Neighbourhood_Watch
-  --if homemonitor.NeighbourhoodWatchInArea = '1' then 'Y' else '0' end as Neighbourhood_Watch
   ,CASE WHEN homemonitor.NeighbourhoodWatchInArea = '1' THEN 'Y' ELSE 'N' END as Neighbourhood_Watch
   ,homemonitor.NumberOfSmokeAlarms as Smoke_Detectors
-    --if homemonitor.NumberOfSmokeAlarms = ' NSAI Approved Installer to EN50131 Standard to Central Station' or 'Installed to EN50131 Standard (Not Connected to Central Station)' or 'PSA/NSAI Approved Installer to EN50131 Standard to Central Station' 
-  --then 'Standard Alarm' else if 'other' then 'Other' else null end as Alarm_Type
+  
+
   ,CASE WHEN homemonitor.RoofConstructionType = 'NSAI Approved Installer to EN50131 Standard to Central Station' THEN 'Standard Alarm' 
   WHEN homemonitor.AlarmType = 'Installed to EN50131 Standard (Not Connected to Central Station)' THEN 'Standard Alarm' 
   WHEN homemonitor.AlarmType = 'PSA/NSAI Approved Installer to EN50131 Standard to Central Station' THEN 'Standard Alarm' 
@@ -150,36 +77,10 @@ SELECT   distinct homemonitor. Customerid
   ,homemonitor.JewelleryAmount as Specified_Items_Jewellery
 
 
+
+
   FROM [OP].[OP].[NBHomeMonitor] homemonitor
-WHERE StartDate >= '2022-10-01' AND StartDate < '2022-11-30'
-  -- AND CustomerID = '2183'
+-- WHERE StartDate >= '2022-10-01' AND StartDate < '2022-11-30'
+where homemonitor.PolicyAddon = 'Buildings Accidental Damage'
+and StartDate >= '2022-01-01' AND StartDate < '2022-11-30'
 
--- 	,claiminfo1.min1 as FirstClaimDate
--- 	,claiminfo1.amount1 as FirstClaimAmount
--- 	 ,claiminfo1.claimtype1 as FirstClaimType
---      ,claiminfo1.claimstatus1 as FirstClaimStatus
---     ,claiminfo1.occurredat1 as FirstClaimPropertyOccurredAt
-
--- 	,claiminfo2.min2 as SecondClaimDate
--- 	,claiminfo2.amount2 as SecondClaimAmount
--- 	,claiminfo2.claimtype2 as SecondClaimType
--- 	,claiminfo2.claimstatus2 as SecondClaimStatus
--- 	,claiminfo2.occurredat2 as SecondClaimPropertyOccurredAt
-
--- 	,claiminfo3.min3 as ThirdClaimDate
--- 	,claiminfo3.amount3 as ThirdClaimAmount
--- 	,claiminfo3.claimtype3 as ThirdClaimType
--- 	,claiminfo3.claimstatus3 as ThirdClaimStatus
--- 	,claiminfo3.occurredat3 as ThirdClaimPropertyOccurredAt
-
--- 	,claiminfo4.min4 as FourthClaimDate
--- 	,claiminfo4.amount4 as FourthClaimAmount
--- 	,claiminfo4.claimtype4 as FourthClaimType
--- 	,claiminfo4.claimstatus4 as FourthClaimStatus
--- 	,claiminfo4.occurredat4 as FourthClaimPropertyOccurredAt
-
--- 	,claiminfo5.min5 as FifthClaimDate
--- 	,claiminfo5.amount5 as FifthClaimAmount
--- 	,claiminfo5.claimtype5 as FifthClaimType
--- 	,claiminfo5.claimstatus5 as FifthClaimStatus
--- 	,claiminfo5.occurredat5 as FifthClaimPropertyOccurredAt
